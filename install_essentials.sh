299a5b91173658c0fa16468cf80206a9c4ae8742
TheBauwssss <TheBauwssss@users.noreply.github.com>
2017-12-16 23:18:10 +0100

    Added essentials script

---
+++ install_essentials.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+if [ $# -eq 0 ]
+  then
+    echo "No arguments supplied! Updating apt..."
+	apt update
+fi
+
+echo "Installing essentials..."
+
+apt install htop locate nano unzip screen tree -y
+
+echo ""
+echo "Enabling ssh root login with password..."
+
+echo "PermitRootLogin yes" | tee -a /etc/ssh/sshd_config
+echo "PasswordAuthentication yes" | tee -a /etc/ssh/sshd_config
+
+service sshd restart
+service ssh restart
